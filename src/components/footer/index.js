8f8b4e84b960c0234a17f54685ee7b499c4f649d
Sierra MacDonald <sierrainertia@gmail.com>
2021-03-24 21:13:04 -0400

    finish footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,64 +4,64 @@ import "./index.scss";
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="wrapper">
-        <section className="footer__links">
-          <div className="footer__logo">
-            <span>Moon Munchies</span>
-            <span>Freeze-dried Treats</span>
-          </div>
-          <div>
-            <header>Links</header>
-            <ul>
-              <li>
-                <a href="#home">Home</a>
-              </li>
-              <li>
-                <a href="#about">About us</a>
-              </li>
-              <li>
-                <a href="#products">Products</a>
-              </li>
-            </ul>
-          </div>
+      <section className="footer__links">
+        <div className="footer__logo">
+          <span className="moon-munchies">Moon Munchies</span>
+          <span>Freeze-dried Treats</span>
+        </div>
+        <div>
+          <header>Links</header>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About us</a>
+            </li>
+            <li>
+              <a href="#products">Products</a>
+            </li>
+          </ul>
+        </div>
 
-          <div>
-            <header>Follow us</header>
-            <ul className="footer__social-links">
-              <li>
-                <a
-                  href="https://www.facebook.com/MoonMunchies"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-facebook"></i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://instagram.com/moon_munchies_and_beyond?igshid=17yssd3t95xun"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-instagram-square"></i>
-                </a>
-              </li>
-              <li>
-                <a href="mailto:info@moonmunchies.ca">
-                  <i className="far fa-envelope"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <section className="footer__copyright">
-          <p>Copyright &copy; Moon Munchies {new Date().getFullYear()}</p>
-          <p>
-            Website made with ❤️ by
-            <a href="https://sierracodes.online"> Sierra MacDonald</a>
-          </p>
-        </section>
-      </div>
+        <div>
+          <header>Follow us</header>
+          <ul className="footer__social-links">
+            <li>
+              <a
+                href="https://www.facebook.com/MoonMunchies"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fab fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://instagram.com/moon_munchies_and_beyond?igshid=17yssd3t95xun"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="mailto:info@moonmunchies.ca">
+                <i className="far fa-envelope"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className="footer__copyright">
+        <p className="copyright">
+          Copyright &copy; Moon Munchies {new Date().getFullYear()}
+        </p>
+        <p>
+          Website made with ❤️ by
+          <a href="https://sierracodes.online"> Sierra MacDonald</a>
+        </p>
+      </section>
     </footer>
   );
 };
