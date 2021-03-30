bdf2b5a5c76782cca92852caef16f2016e7681f2
Abe Fehr <abe.fehr@gmail.com>
2021-03-29 23:09:41 -0400

    Make hero fancy

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import heroImage from "../../images/hero-image.png";
 import "./index.scss";
 
 export const Hero = () => {
   return (
     <div className="hero" id="home">
       <div className="wrapper">
-        <div className="hero__image"></div>
+        <div className="hero__image">
+          <img src={heroImage} />
+        </div>
         <div className="hero__text">
           <h1>Freeze-dried Treats</h1>
           <p>Flavour that will take you to the moon and beyond.</p>
