26eb97cf981618de351fd5579155675de94bfedc
Sierra MacDonald <sierrainertia@gmail.com>
2021-08-05 23:15:57 -0600

    add locations

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -8,14 +8,11 @@ export const About = () => {
         <div className="info">
           <h2>About us</h2>
           <p>
-            {/* We’re a family run business with a mission to create powerful and
-            unique food experiences. By freeze-drying */}
-            Moon Munchies creates freeze-dried candy, fruits, and much more! By
-            doing this we intensify the flavours and provides a whole new way to
-            enjoy your snacks.
+            At Moon Munchies we freeze-dry your favourite candy, fruits, and
+            much more! By doing this we intensify the flavours and provide a
+            whole new way to enjoy your snacks.
           </p>
           <p>Does your favourite confectionary send you to the dentist? </p>
-
           <p>
             Do grandma and grandpa&apos;s dentures pop out every time they take
             a bite of their delectable desserts?
@@ -28,6 +25,81 @@ export const About = () => {
             Ready to try flavours that are out of this world? Place an order
             today!
           </p>
+          <p>
+            Need your fix? Moon Munchies are available for pick up and delivery.
+            Place an order today or find us at any of the below locations:
+          </p>
+          <ul>
+            <li>
+              <a href="https://g.page/homegrowninab?share" target="_blank">
+                Home Grown | Airdrie, AB
+              </a>
+            </li>
+            <li>
+              <a href="https://g.page/Shoplift-Deals?share" target="_blank">
+                Shoplift Deals | Airdrie, AB
+              </a>
+            </li>
+            <li>
+              <a href="https://goo.gl/maps/rrrEwE8KZTmhi7Zt8" target="_blank">
+                Squiggly Pig Marketplace | Airdrie, AB
+              </a>
+            </li> 
+            <li>
+              <a href="https://goo.gl/maps/xJhh6GCr3YGRC6qo6" target="_blank">
+              Olde Tyme Candy Shoppe | Canmore, AB
+              </a>
+            </li>
+            <li>
+              <a href="https://g.page/locallovegiftingco?share" target="_blank">
+                Local Love Gifting Co | Calgary, AB
+              </a>
+            </li> 
+            <li>
+              <a href="https://goo.gl/maps/XAAfo3Xzfr4aVUTZ6" target="_blank">
+                Thistle and Clover | Calgary, AB
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://g.page/the-donut-man-crossfield?share"
+                target="_blank"
+              >
+                The Donut Man | Crossfield, AB
+              </a>
+            </li>
+            <li>
+              <a href="https://g.page/KoffeeCup?share" target="_blank">
+                Netty's Koffee Cup | Lethbridge, AB
+              </a>
+            </li>
+            <li>
+              <a href="https://goo.gl/maps/qBpm3rcbs2Mxgj51A" target="_blank">
+                Kim & CO. cafe & gift shop | Milk River, AB
+              </a>
+            </li>
+            <li>
+              <a href="https://g.page/the-hive-nanton?share" target="_blank">
+                The Hive Nanton | Nanton, AB
+              </a>
+            </li> 
+            <li>
+              <a href="https://goo.gl/maps/JNVgZzZdQaP5CV8H7" target="_blank">
+              The Chic Crafter | Redcliff, AB
+              </a>
+            </li>
+            <li>
+              <a href="https://goo.gl/maps/JBgwEGW7pQyat9h98" target="_blank">
+                Vessel Woodworks | Red Deer, AB
+              </a>
+            </li>
+            <li>
+              <a href="https://goo.gl/maps/6b2En4qJ1Qguv1pA6" target="_blank">
+                Farm Basket | Three Hills, AB
+              </a>
+            </li>
+           
+          </ul>
         </div>
       </div>
     </div>
