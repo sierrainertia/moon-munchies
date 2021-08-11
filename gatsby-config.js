03f3d389edaf140afde9585d21b24a1b5e1f5d53
Abe Fehr <abe.fehr@gmail.com>
2021-08-10 21:31:21 -0400

    Cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,6 @@
 const fs = require("fs");
 const dotenv = require("dotenv");
 
-dotenv.config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
 if (process.env.CONTEXT !== "production") {
   console.warn(`Using the .env.development configuration file`);
   const envConfig = dotenv.parse(fs.readFileSync(".env.development"));
