d2f56f592e7018905e810241fa72433810ec3732
Abe Fehr <abe.fehr@gmail.com>
2022-01-04 19:27:27 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ const IndexPage = ({ data }) => {
       <title>Moon Munchies</title>
       <div className="banner">
         <p>
-          Due to holiday demand, any new orders can only be filled in January
+          Spend $98 for free shipping! Or enjoy free local pickup in Airdrie, AB.
         </p>
       </div>
       <Header />
